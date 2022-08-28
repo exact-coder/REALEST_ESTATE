b2f493f4c80514f9ef61709d5f32ce7d6b2abd81
Programming Bees <hasanakash799@gmail.com>
2022-08-28 16:49:46 +0600

    chore: react project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NotFound from './components/NotFound';
+import About from './containers/About';
+import Contact from './containers/Contact';
+import Home from './containers/Home';
+import ListingDetail from './containers/ListingDetail';
+import Listings from './containers/Listings';
+import Login from './containers/Login';
+import SingUp from './containers/SignUp';
+import Layout from './hocs/Layout';
 
-function App() {
+const App = () => {
   return (
-    <div className="App" style={{textAlign:'center'}}>
-      <h1>This Is a Realest Estate App</h1>
-      <h6>Created By : Jahid Hasan</h6>
-      <h6>Used Technology : React and Django</h6>
-    </div>
-  );
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/contact' element={<Contact/>} />
+          <Route path='/listings' element={<Listings/>} />
+          <Route path='/listings/:id' element={<ListingDetail/>} />
+          <Route path='/signup' element={<SingUp/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='*' element={<NotFound/>}/>
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  )
 }
 
 export default App;
