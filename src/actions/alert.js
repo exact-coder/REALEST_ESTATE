f64c43c5419c3f990bc80f9df165299ab8fd8524
Programming Bees <hasanakash799@gmail.com>
2022-09-02 18:06:41 +0600

    feta: nevbar and routing are complete

--- src/actions/alert.js
+++ src/actions/alert.js
@@ -5,6 +5,7 @@ import { REMOVE_ALERT, SET_ALERT } from './types';
 
 export const setAlert= (msg, alertType, timeout =500) => dispatch => {
     const id = uuid();
+    console.log("Actions alert >>>",id);
     dispatch({
         type: SET_ALERT,
         payload: {msg, alertType, id}
